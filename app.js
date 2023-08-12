f69add8c4d16cf16a6125e5531f5f34742a291f2
Carlos Araujo <vinny.engenharia@outlook.pt>
2023-08-12 04:31:45 -0300

    Adicionando uma linha.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos.") 
 console.log("Aplicação estáse conectando ao banco de dados.")
 console.log("Terceira linha atualizada.")
+console.log("Enviando para o GitHub")
