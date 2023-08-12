4e453cf0bf9afa50b9d6626953a3c1ecdfce5b8f
Carlos Araujo <vinny.engenharia@outlook.pt>
2023-08-12 04:43:13 -0300

    Voltando para o estado do Vinicius

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos.") 
 console.log("Aplicação estáse conectando ao banco de dados.")
 console.log("Terceira linha atualizada.")
-console.log("Enviando para o GitHub")
